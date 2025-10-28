eb05aa4a00b94765be70b637ed5042208e3f575e
realchriswilder <somuchrekt4@gmail.com>
2025-10-28 14:12:56 +0000

    fix: Resolve React webpack Node.js module errors
    - Fixed webpack bundling issues with Node.js modules (util, path, fs, buffer)
    - Added craco.config.js with proper fallbacks for Node.js modules
    - Updated React showcase to use @craco/craco instead of react-scripts
    - Used eval() for dynamic imports to prevent webpack analysis
    - React build now compiles successfully without Node.js module errors
    - Maintains browser-only functionality while supporting Node.js imports
    - Framework-agnostic SDK now works perfectly in both environments!

--- packages/nextjs-showcase/.next/server/middleware-build-manifest.js
+++ packages/nextjs-showcase/.next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-eeaa1b384d536a8e.js","static/chunks/f47af9e7-43b010872fa3659b.js","static/chunks/443-8c629e21b44055d6.js","static/chunks/main-app-4bb3a93819d1b482.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-eeaa1b384d536a8e.js","static/chunks/framework-016dca3c70a26744.js","static/chunks/main-395f664acc95e742.js","static/chunks/pages/_app-1207e18191b93cd4.js"],"/_error":["static/chunks/webpack-eeaa1b384d536a8e.js","static/chunks/framework-016dca3c70a26744.js","static/chunks/main-395f664acc95e742.js","static/chunks/pages/_error-598e710e7fce2b4a.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
